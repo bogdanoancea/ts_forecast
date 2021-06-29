2b53bdd77dcd17c0225e54502258b163607b0799
bogdanoancea <bogdan.oancea@gmail.com>
2021-06-29 15:32:59 +0300

    develop

--- prelTS.R
+++ prelTS.R
@@ -34,6 +34,52 @@ industry_test <- industry[47:51,]
 retail_test <- retail[47:51,]
 
 # Exponential smoothing
-etsfit_construction <- ets(construction_fit)
-etsfit_industry <- ets(industry_fit)
-etsfit_retail <- ets(retail_fit)
+acc_ets_construction <- data.frame()
+for(i in 1:20) {
+  tc <- ts(construction_fit[,i+1], frequency = 12, start = c(2017,1))
+  etsfit_construction <- ets(tc)
+  test <- forecast(tc, model = etsfit_construction, use.initial.values=TRUE, h = 5)
+  acc_ets_construction <- rbind(acc_ets_construction, av.res(as.data.frame(construction_test[,i+1]), as.data.frame(test$mean)))
+}
+
+
+acc_ets_industry <- data.frame()
+for(i in 1:29) {
+  ti <- ts(industry[,i+1], frequency = 12, start = c(2017,1))
+  etsfit_industry <- ets(ti)
+  test <- forecast(ti, model = etsfit_industry, use.initial.values=TRUE, h = 5)
+  acc_ets_industry <- rbind(acc_ets_industry, av.res(as.data.frame(industry_test[,i+1]), as.data.frame(test$mean)))
+}
+
+acc_ets_retail <- data.frame()
+for(i in 1:28) {
+  tr <- ts(retail[,i+1], frequency = 12, start = c(2017,1))
+  etsfit_retail <- ets(tr)
+  test <- forecast(tr, model = etsfit_retail, use.initial.values=TRUE, h = 5)
+  acc_ets_retail <- rbind(acc_ets_retail, av.res(as.data.frame(retail_test[,i+1]), as.data.frame(test$mean)))
+}
+
+# ARIMA
+acc_ARIMA_construction <- data.frame()
+for(i in 1:20) {
+  tc <- ts(construction_fit[,i+1], frequency = 12, start = c(2017,1))
+  ARIMAfit_construction <- auto.arima(tc)
+  test <- forecast(tc, model = ARIMAfit_construction, use.initial.values=TRUE, h=5)
+  acc_ARIMA_construction <- rbind(acc_ARIMA_construction, av.res(as.data.frame(construction_test[,i+1]), as.data.frame(test$mean)))
+}
+
+acc_ARIMA_industry <- data.frame()
+for(i in 1:20) {
+  tc <- ts(industry_fit[,i+1], frequency = 12, start = c(2017,1))
+  ARIMAfit_industry <- auto.arima(tc)
+  test <- forecast(tc, model = ARIMAfit_industry, use.initial.values=TRUE, h=5)
+  acc_ARIMA_construction <- rbind(acc_ARIMA_construction, av.res(as.data.frame(construction_test[,i+1]), as.data.frame(test$mean)))
+}
+
+acc_ARIMA_retail <- data.frame()
+for(i in 1:20) {
+  tc <- ts(industry_fit[,i+1], frequency = 12, start = c(2017,1))
+  ARIMAfit_industry <- auto.arima(tc)
+  test <- forecast(tc, model = ARIMAfit_industry, use.initial.values=TRUE, h=5)
+  acc_ARIMA_construction <- rbind(acc_ARIMA_construction, av.res(as.data.frame(construction_test[,i+1]), as.data.frame(test$mean)))
+}
